6d3fb708cc1c2ffa1245b7ae6b15865245a33647
philipprobin <philipprobinsowik@gmail.com>
2020-08-27 15:53:26 +0200

    Abstandsmatrix doch nicht feritg

--- aahhh_shit_here_we_go_again.py
+++ aahhh_shit_here_we_go_again.py
@@ -43,7 +43,7 @@ def plot_weighted_graph(node_list,pos):
     nx.draw_networkx_edges(G,pos)
     nx.draw_networkx_edge_labels(G,pos,edge_labels={(node_list[counter],copy[counter]):weite[counter]})
 
-    
+    weite.insert(len(weite),None)
     plt.draw()
     plt.show()
     print(node_list)
@@ -63,26 +63,55 @@ def umschreiben(node_list):
     x = numbers.index(24)
     numbers[x] = 0
 
+    # numbers ums depot erweitern
+    numbers.insert(len(numbers),7)
+    print(numbers)
+
     print(numbers)
     return numbers
 
 
-def hilfsgraph(weite, numbers):
+def hilfsgraph(weite, numbers, ab):
     
+    #none setzen array
     ha = np.zeros(shape=(len(numbers),len(numbers)))
 
     for i in range (len(numbers)):
         for j in range (len(numbers)):
             ha[i][j] = None
 
+    #befüllen der Grade
     for i in range (len(numbers)-1):
         ha[numbers[i]][numbers[i+1]]=weite[i]
     ha[numbers[-1]][numbers[0]] = weite[-1]
 
+    
+    
+    #minIndex = position von j speichern
+
+    for i in range(len(numbers)-2):
+        for k in range(i+2, len(numbers)):
+            #d ist der kürzeste weg von allen j verschiebungen, mit festem i und k
+            #dt ist der weg vom temporären j
+            d = 399
+            dt = 400
+            for j in range(i+1, len(numbers)):
+                if(numbers[j] == numbers[k]):
+                    break
+                #addieren beider dronenrouten
+                dt = ab[numbers[i]][numbers[j]] + ab[numbers[j]][numbers[k]] # +Kostenberechnung
+                if(dt<d):
+                    d = dt
+            #1000 ist die weiteste entfernung die die Drohne fliegen könnte (Limit)
+            if(d<300):
+                ha[numbers[i]][numbers[k]]= d
+            else:
+                ha[numbers[i]][numbers[k]]= 9999 #unendlich
+    print('ha = ')
     print(ha)
     return ha
 
-
+#abstandsmatrix alleknoten
 def abstaende(numbers):
     ab = np.zeros(shape=(len(numbers),len(numbers)))
 
@@ -106,18 +135,14 @@ def abstaende(numbers):
     return ab
     
 
-
-
-
-
 node_list = ['X','A','B','C','D','E','F'] 
 pos={'X':(0,0),'A':(220,20),'B':(270,70),'C':(250,210),'D':(90,60),'E':(120,120),'F':(50,220)}
-posnumbers={'0':(0,0),'1':(220,20),'2':(270,70),'3':(250,210),'4':(90,60),'5':(120,120),'6':(50,220)}
+posnumbers={'0':(0,0),'1':(220,20),'2':(270,70),'3':(250,210),'4':(90,60),'5':(120,120),'6':(50,220), '7':(0,0)}
 
 weite,node_list=plot_weighted_graph(node_list,pos)
 numbers=umschreiben(node_list)
-ha = hilfsgraph(weite,numbers)
 ab= abstaende(numbers)
+ha = hilfsgraph(weite,numbers,ab)
 print(ab)
 
 
