043789d4c20b8b3b4187e2051ec9b7aa6afa8579
philipprobin <philipprobinsowik@gmail.com>
2020-08-28 14:31:11 +0200

    Abstandsmatrix + Hilfsgraph

--- aahhh_shit_here_we_go_again.py
+++ aahhh_shit_here_we_go_again.py
@@ -6,8 +6,8 @@ import math
  
 def plot_weighted_graph(node_list,pos):
     
-
-    G = nx.Graph() 
+    G = nx.MultiDiGraph(format='png', directed=True)
+    
     
     #random nodelist
     
@@ -37,6 +37,7 @@ def plot_weighted_graph(node_list,pos):
         weite[counter]=round(weite[counter],1)
         nx.draw_networkx_edge_labels(G,pos,edge_labels={(node_list[counter],copy[counter]):weite[counter]})
 
+
     #draws
     nx.draw_networkx_nodes(G,pos)
     nx.draw_networkx_labels(G,pos,font_size=10, font_family="sans-serif")
@@ -44,8 +45,8 @@ def plot_weighted_graph(node_list,pos):
     nx.draw_networkx_edge_labels(G,pos,edge_labels={(node_list[counter],copy[counter]):weite[counter]})
 
     weite.insert(len(weite),None)
-    plt.draw()
-    plt.show()
+    #plt.draw()
+    #plt.show()
     print(node_list)
     print(weite)
 
@@ -75,7 +76,8 @@ def hilfsgraph(weite, numbers, ab):
     
     #none setzen array
     ha = np.zeros(shape=(len(numbers),len(numbers)))
-
+    print(len(ha[0]))
+    print(len(ha))
     for i in range (len(numbers)):
         for j in range (len(numbers)):
             ha[i][j] = None
@@ -133,7 +135,64 @@ def abstaende(numbers):
         for b in range (len(numbers)):
             ab[a][b]= round(math.hypot( round(abs( xcoord[a]-xcoord[b]  ),1) ,round(abs(  ycoord[a]-ycoord[b] ),1) ),1)
     return ab
+
+def test(ha):
+    edges=[]
+    for i in range (len(ha)):
+        for j in range (len(ha)):
+            if ha[i][j]!=None:
+                edges.append((str(i),str(j)))
+    print(edges)
+
+def print_hilfsgraph(ha):
+    # Graph data
+
+    #zB [0, 1, 2, 3, 6, 4, 5, 7]
+    names = []
+    for number in numbers:
+        names.append(str(number))
+    positions = [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0)]
+    edges = []
+
     
+    # Matplotlib figure
+    plt.figure('Hilfsgraph')
+
+    # Create graph
+    G = nx.MultiDiGraph(format='png', directed=True)
+
+    for index, name in enumerate(names):
+        G.add_node(name, pos=positions[index])
+
+
+
+    #ha durchlaufen, kanten hinzufügen, beschriftungen hinzufügen
+    for i in range (len(ha)):
+        for j in range (len(ha)):
+            if ha[i][j]>0:
+                edges.append((str(j),str(i)))
+                #nx.draw_networkx_edge_labels(G,positions,edge_labels={(i,j):ha[i][j]})
+
+
+    
+    layout = dict((n, G.nodes[n]["pos"]) for n in G.nodes())
+    nx.draw(G, pos=layout, with_labels=True, node_size=300)
+    ax = plt.gca()
+    for edge in edges:
+        ax.annotate("",
+                    xy=layout[edge[0]], xycoords='data',
+                    xytext=layout[edge[1]], textcoords='data',
+                    arrowprops=dict(arrowstyle="->", color="0.5",
+                                    shrinkA=5, shrinkB=5,
+                                    patchA=None, patchB=None,
+                                    connectionstyle="arc3,rad=-0.7",
+                                    ),
+                    )
+    
+    nx.draw_networkx_labels(G,posnumbers,font_size=1, font_family="sans-serif")
+    plt.draw()
+    plt.show()
+
 
 node_list = ['X','A','B','C','D','E','F'] 
 pos={'X':(0,0),'A':(220,20),'B':(270,70),'C':(250,210),'D':(90,60),'E':(120,120),'F':(50,220)}
@@ -144,6 +203,7 @@ numbers=umschreiben(node_list)
 ab= abstaende(numbers)
 ha = hilfsgraph(weite,numbers,ab)
 print(ab)
+print_hilfsgraph(ha)
 
 
 
